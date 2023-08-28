e3e27e399cff8383cebee79804df828261fc484e
Matthew B <31496100+MattLearn@users.noreply.github.com>
2023-08-27 23:54:45 -0500

    Update main.py

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -26,9 +26,23 @@ for (index, row) in student_data_frame.iterrows():
 NATO_alphabet_file = pandas.read_csv("nato_phonetic_alphabet.csv")
 NATO_alphabet_data = NATO_alphabet_file.iterrows()
 NATO_dict = {row.letter:row.code for (index, row) in NATO_alphabet_data}
-print(NATO_dict)
+# print(NATO_dict)
 
 # 2. Create a list of the phonetic code words from a word that the user inputs.
-name = [n for n in input("Enter a name: ").upper()]
-Nato_spelling = [NATO_dict[letter] for letter in name if letter in NATO_dict]
-print(Nato_spelling)
+# name = [n for n in input("Enter a name: ").upper()]
+# Nato_spelling = [NATO_dict[letter] for letter in name if letter in NATO_dict]
+# print(Nato_spelling)
+
+# a redo of number 2 with error handling implemented
+def generate_phonetic():
+    name = input("Enter a name: ").upper()
+    try:
+        nato_spelling = [NATO_dict[letter] for letter in name]
+    except KeyError:
+        print("Input Error: Only letters in the alphabet are accepted")
+        generate_phonetic()
+    else:
+        print(nato_spelling)
+
+
+generate_phonetic()
